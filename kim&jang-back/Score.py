4065d50103852f9e9d31791dd815317339db7f3e
SeongWonKim <54873618+Won-Seong@users.noreply.github.com>
2022-01-23 09:56:42 +0900

    Score update

--- kim&jang-back/Score.py
+++ kim&jang-back/Score.py
@@ -2,6 +2,7 @@ from RandomConnectionSolution import Student
 import numpy as np
 from tabulate import tabulate
 from dbInfo import cursor
+from collections import OrderedDict
 
 def mbti_score(mbti_one, mbti_two):
     sql = f"SELECT * FROM mbti_score WHERE mbti_one = '{mbti_one}' and mbti_two = '{mbti_two}';"
@@ -30,14 +31,31 @@ def two_student_score(student_1, student_2):
     if(student_1["place"] == student_2["place"]) : result += 1
     return result
      
-def score_algorithm(set_of_student_id):
-    stu_list = Student.set_student_info(set_of_student_id)
+def score_algorithm(list_of_student_id):
+    stu_list = Student.list_student_info(list_of_student_id)
     matrix = np.zeros([len(stu_list), len(stu_list)])
     for i in range(len(stu_list)):
         for j in range(len(stu_list)):
             if i == j : continue        
             matrix[i][j] = two_student_score(stu_list[i] , stu_list[j])
-    return matrix
+    return matrix, list_of_student_id
+
+def greedy_score(matrix_of_score, stu_list):
+    matching_dict = dict()
+    ordered_dict = dict()
+    visit = {i : False for i in range(len(stu_list))}
+    for i in range(len(stu_list)) :
+        for j in range( len(stu_list) ): 
+            ordered_dict[matrix_of_score[i][j]] = (i, j)
+    for i, j in ordered_dict.values() :
+        if len(matching_dict) >= len(stu_list) / 2 : break
+        elif i == j : pass
+        elif visit[i] == True or visit[j] == True : pass
+        else : 
+            matching_dict[stu_list[i]] = stu_list[j]
+            visit[i] = True
+            visit[j] = True
+    return matching_dict
 
 if  __name__ == "__main__":
-    print(score_algorithm({'11112222','11113333'}))
+   print(greedy_score(*score_algorithm(['11112222','11113333'])))
